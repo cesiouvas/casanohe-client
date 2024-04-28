6f1fdb5c7a236830a0d448aef1ae9cae5fb5dc8a
sergio castillo <sergiocastillorens@gmail.com>
2024-04-28 21:03:23 +0200

    añadir producto a cesta

--- js/main.js
+++ js/main.js
@@ -45,8 +45,13 @@ function printHEader() {
             <p class="nav-menu"><a href="${src}faq/faq.html">Dudas</a></p>
             <p class="nav-menu"><a href="${src}about/about.html">Sobre mí</a></p>
         </div>
-        <button class="rounded-circle menu-buttons">
-            <a id="profile" href="${src}profile/login.html"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256"><path fill="#ffffff" d="M230.93 220a8 8 0 0 1-6.93 4H32a8 8 0 0 1-6.92-12c15.23-26.33 38.7-45.21 66.09-54.16a72 72 0 1 1 73.66 0c27.39 8.95 50.86 27.83 66.09 54.16a8 8 0 0 1 .01 8"/></svg></a>
-        </button>
+        <div>
+            <button class="rounded-circle menu-buttons">
+                <a id="profile" href="${src}profile/login.html"><svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 256 256"><path fill="#ffffff" d="M230.93 220a8 8 0 0 1-6.93 4H32a8 8 0 0 1-6.92-12c15.23-26.33 38.7-45.21 66.09-54.16a72 72 0 1 1 73.66 0c27.39 8.95 50.86 27.83 66.09 54.16a8 8 0 0 1 .01 8"/></svg></a>
+            </button>
+            <button class="rounded-circle menu-buttons">
+                Carrito
+            </button>
+        </div>
     </div>`
 }
--- products/js/product.js
+++ products/js/product.js
@@ -1,4 +1,87 @@
 let paramURL = new URLSearchParams(window.location.search)
-let id = paramURL.get('idProd')
+let product_id = paramURL.get('idProd')
 
-console.log(id);
+// token de sesión
+let tokenusu = sessionStorage.getItem('tokenusu')
+
+let img = document.getElementById('img')
+let content = document.getElementById('content')
+
+$(window).on('load', function () {
+    getProductDetails()
+})
+
+function getProductDetails() {
+    $.ajax({
+        type: "GET",
+        url: 'http://localhost:8000/api/getProductDetails',
+        dataType: "json",
+        data: {
+            product_id: product_id,
+        },
+        success: function (response) {
+            let data = response.data;
+
+            img.innerHTML = `<img class="w-100" src="../img/${data.image}.png" alt="imagen ${data.image}">`
+
+            content.innerHTML += `<p>${data.name}</p>
+                <p>${data.desc}</p>
+                <p>${data.price} €</p>
+                <div class="quantity-selector">
+                    <button id="decrease"><i class="fas fa-minus">-</i></button>
+                    <input type="number" id="quantity" value="0" min="0">
+                    <button id="increase"><i class="fas fa-plus">+</i></button>
+                </div>
+                <button id="addProduct" class="btn btn-primary">Añadir a la cesta</button>`
+
+            createQuantityButton()
+
+            addProduct();
+        },
+    })
+}
+
+function createQuantityButton() {
+    const decreaseBtn = document.getElementById('decrease');
+    const increaseBtn = document.getElementById('increase');
+    const quantityInput = document.getElementById('quantity');
+
+    decreaseBtn.addEventListener('click', function () {
+        let currentValue = parseInt(quantityInput.value);
+        if (currentValue > 0) {
+            quantityInput.value = currentValue - 1;
+        }
+    });
+
+    increaseBtn.addEventListener('click', function () {
+        let currentValue = parseInt(quantityInput.value);
+        quantityInput.value = currentValue + 1;
+    });
+
+}
+
+// añadir product a la cesta
+function addProduct() {
+    $('#addProduct').on('click', function () {
+        console.log(tokenusu);
+        let quantity = $('#quantity').val()
+        console.log(quantity);
+        console.log(product_id);
+
+        $.ajax({
+            type: "POST",
+            url: 'http://localhost:8000/api/addProductToCart',
+            dataType: "json",
+            headers: {
+                Authorization: 'Bearer ' + tokenusu
+            },
+            data: {
+                quantity: quantity,
+                product_id: product_id
+            },
+            success: function (response) {
+               console.log("ano");
+            },
+        })
+    })
+}
