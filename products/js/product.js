4b5f3ba77905e663bdf819af8e91d431f0cf2ef3
sergio castillo <sergiocastillorens@gmail.com>
2024-05-19 18:16:48 +0200

    mensajes en producto

--- products/js/product.js
+++ products/js/product.js
@@ -4,6 +4,7 @@ import { route } from '../../js/main.js'
 
 let paramURL = new URLSearchParams(window.location.search)
 let product_id = paramURL.get('idProd')
+let comprobarStock
 
 // token de sesión
 let tokenusu = sessionStorage.getItem('tokenusu')
@@ -26,20 +27,42 @@ function getProductDetails() {
         },
         success: function (response) {
             let data = response.data;
+            let color
+            let stock
+
+            comprobarStock = data.quantity
 
             img.innerHTML = `<img class="w-100" src="../img/${data.image}.png" alt="imagen ${data.image}">`
 
             let cad = `<h4>${data.name}</h4>
-                <p class="pb-4">${data.desc}</p>
+                <p>${data.desc}</p>`
+            // cambiar colores según cantidades
+            switch (true) {
+                case (data.quantity > 5):
+                    color = '#CED93C'
+                    stock = data.quantity
+                    break;
+                case (data.quantity < 5):
+                    color = '#FFB536'
+                    stock = data.quantity
+                    break;
+                case (data.quantity == 0):
+                    color = 'red'
+                    stock = 'Nou hay stock'
+                    break;
+            }
+
+            cad += `<p><b>Stock: </b><b style="color: ${color};">${stock}</b></p>
                 <h5 class="text-end">${data.price} €</h5>
                 <div class="quantity-selector text-center">
                     <button id="decrease"><i class="fas fa-minus">-</i></button>
                     <input type="number" id="quantity" value="0" min="0">
                     <button id="increase"><i class="fas fa-plus">+</i></button>
                 </div>`
-    
+
             $('#content').append(cad)
 
+            // asignar funciones de añadir y restar cantidad
             createQuantityButton()
 
             if (!tokenusu) {
@@ -49,6 +72,8 @@ function getProductDetails() {
                     window.location.replace('../profile/login.html')
                 })
             } else {
+                // aparece personalizar
+                $('#createCustom').attr('hidden', false)
                 addProduct()
             }
         },
@@ -61,6 +86,8 @@ function createQuantityButton() {
     const increaseBtn = document.getElementById('increase')
     const quantityInput = document.getElementById('quantity')
 
+    $('#quantity').attr('max', comprobarStock);
+
     decreaseBtn.addEventListener('click', function () {
         let currentValue = parseInt(quantityInput.value)
         if (currentValue > 0) { // no posible menor que 0
@@ -70,7 +97,9 @@ function createQuantityButton() {
 
     increaseBtn.addEventListener('click', function () {
         let currentValue = parseInt(quantityInput.value)
-        quantityInput.value = currentValue + 1
+        if (currentValue < comprobarStock) { // la cantidad no podrá ser mayor que el stock
+            quantityInput.value = currentValue + 1
+        }
     });
 }
 
@@ -79,28 +108,43 @@ function addProduct() {
     $('#addProduct').on('click', function () {
         let quantity = $('#quantity').val()
 
-        $.ajax({
-            type: "POST",
-            url: route + 'addProductToCart',
-            dataType: "json",
-            headers: {
-                Authorization: 'Bearer ' + tokenusu
-            },
-            data: {
-                quantity: quantity,
-                product_id: product_id
-            },
-            success: function (response) {
-                // recargar carrito
-                llenarCarrito()
-            },
-        })
+        if (quantity >= 1 && quantity <= comprobarStock) {
+            $.ajax({
+                type: "POST",
+                url: route + 'addProductToCart',
+                dataType: "json",
+                headers: {
+                    Authorization: 'Bearer ' + tokenusu
+                },
+                data: {
+                    quantity: quantity,
+                    product_id: product_id
+                },
+                success: function (response) {
+                    // recargar carrito
+                    llenarCarrito()
+                    $("#productoAnyadido").removeAttr("hidden");
+                    setTimeout(() => {
+                        $("#productoAnyadido").attr("hidden", true);
+                    }, 5000);
+                },
+            })
+        } else if (quantity == 0) {
+            $("#errorCantidad").removeAttr("hidden");
+            setTimeout(() => {
+                $("#errorCantidad").attr("hidden", true);
+            }, 5000);
+        } else {
+            $("#errorCantidadMax").removeAttr("hidden");
+            setTimeout(() => {
+                $("#errorCantidadMax").attr("hidden", true);
+            }, 5000);
+        }
     })
 }
 
 function cargarModalCustom() {
     $('#product_id').val(product_id)
-
 
     $('#confirmarCustom').on('click', function () {
 
